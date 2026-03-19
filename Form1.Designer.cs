37add2dc732ca5622f04a7cd05f5b9930e1a2ad7
DiamondDogV <diamonddog64v@gmail.com>
2026-03-19 10:21:12 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WitcherInventory
+{
+    partial class WitcherInventory
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WitcherInventory));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SwordButton = new System.Windows.Forms.Button();
+            this.BombButton = new System.Windows.Forms.Button();
+            this.PotionButton = new System.Windows.Forms.Button();
+            this.ArmorButton = new System.Windows.Forms.Button();
+            this.TextBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 26F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 61;
+            this.listBox1.Location = new System.Drawing.Point(71, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1068, 1220);
+            this.listBox1.TabIndex = 0;
+            // 
+            // SwordButton
+            // 
+            this.SwordButton.BackColor = System.Drawing.Color.Black;
+            this.SwordButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SwordButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SwordButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.SwordButton.FlatAppearance.BorderSize = 2;
+            this.SwordButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.SwordButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.SwordButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SwordButton.Location = new System.Drawing.Point(1409, 198);
+            this.SwordButton.Name = "SwordButton";
+            this.SwordButton.Size = new System.Drawing.Size(115, 115);
+            this.SwordButton.TabIndex = 1;
+            this.SwordButton.UseVisualStyleBackColor = false;
+            // 
+            // BombButton
+            // 
+            this.BombButton.BackColor = System.Drawing.Color.Black;
+            this.BombButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BombButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BombButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BombButton.FlatAppearance.BorderSize = 2;
+            this.BombButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.BombButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.BombButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BombButton.Location = new System.Drawing.Point(1957, 257);
+            this.BombButton.Name = "BombButton";
+            this.BombButton.Size = new System.Drawing.Size(115, 115);
+            this.BombButton.TabIndex = 2;
+            this.BombButton.UseVisualStyleBackColor = false;
+            // 
+            // PotionButton
+            // 
+            this.PotionButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PotionButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PotionButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PotionButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.PotionButton.FlatAppearance.BorderSize = 2;
+            this.PotionButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.PotionButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.PotionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PotionButton.Location = new System.Drawing.Point(2028, 834);
+            this.PotionButton.Name = "PotionButton";
+            this.PotionButton.Size = new System.Drawing.Size(115, 115);
+            this.PotionButton.TabIndex = 3;
+            this.PotionButton.UseVisualStyleBackColor = false;
+            // 
+            // ArmorButton
+            // 
+            this.ArmorButton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ArmorButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ArmorButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ArmorButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ArmorButton.FlatAppearance.BorderSize = 2;
+            this.ArmorButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.ArmorButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.ArmorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ArmorButton.Location = new System.Drawing.Point(1223, 738);
+            this.ArmorButton.Margin = new System.Windows.Forms.Padding(0);
+            this.ArmorButton.Name = "ArmorButton";
+            this.ArmorButton.Size = new System.Drawing.Size(237, 522);
+            this.ArmorButton.TabIndex = 4;
+            this.ArmorButton.UseVisualStyleBackColor = false;
+            // 
+            // TextBoxLog
+            // 
+            this.TextBoxLog.BackColor = System.Drawing.Color.Black;
+            this.TextBoxLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxLog.Font = new System.Drawing.Font("Consolas", 12F);
+            this.TextBoxLog.ForeColor = System.Drawing.Color.Yellow;
+            this.TextBoxLog.Location = new System.Drawing.Point(2155, 12);
+            this.TextBoxLog.Multiline = true;
+            this.TextBoxLog.Name = "TextBoxLog";
+            this.TextBoxLog.ReadOnly = true;
+            this.TextBoxLog.Size = new System.Drawing.Size(371, 549);
+            this.TextBoxLog.TabIndex = 5;
+            // 
+            // WitcherInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(2538, 1384);
+            this.Controls.Add(this.TextBoxLog);
+            this.Controls.Add(this.ArmorButton);
+            this.Controls.Add(this.PotionButton);
+            this.Controls.Add(this.BombButton);
+            this.Controls.Add(this.SwordButton);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WitcherInventory";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button SwordButton;
+        private System.Windows.Forms.Button BombButton;
+        private System.Windows.Forms.Button PotionButton;
+        private System.Windows.Forms.Button ArmorButton;
+        private System.Windows.Forms.TextBox TextBoxLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WitcherInventory.Collections;
+using WitcherInventory.Entities;
+using static WitcherInventory.Entities.Weapon;
+
+namespace WitcherInventory
+{
+    public partial class WitcherInventory : Form
+    {
+        private Inventory _inventory;
+
+        private Sword _selectedSword;       // Выбранный меч
+        private Armor _selectedArmor;       // Выбранная броня
+        private Potion _selectedPotion;     // Выбранное зелье
+        private Bomb _selectedBomb;         // Выбранная бомба
+
+        public WitcherInventory()
+        {
+            InitializeComponent();
+
+            // Создаем инвентарь
+            _inventory = new Inventory();
+
+            // Добавляем тестовые предметы
+            _inventory.AddItem(new Sword("Стальной меч", 3.5, 200, 150, DamageType.Steel));
+            _inventory[0].Icon = Properties.Resources.SteelSword;
+            _inventory.AddItem(new Swallow("Ласточка", 0.1, 20, 5));
+            _inventory[1].Icon = Properties.Resources.Swallow;
+            _inventory.AddItem(new Armor("Доспех Ворона", 5.0, 300, 100, 50));
+            _inventory[2].Icon = Properties.Resources.CrowArmor;
+            _inventory.AddItem(new Bomb("Сев. ветер", 0.5, 30, 70, DamageType.Ice, 4));
+            _inventory[3].Icon = Properties.Resources.NorthWind;
+            _inventory.AddItem(new SuperiorCat("Кошка", 0.1, 10, 1));
+            _inventory[4].Icon = Properties.Resources.Cat;
+            _inventory.AddItem(new Sword("Серебряный меч", 3.0, 300, 120, DamageType.Silver));
+            _inventory[5].Icon = Properties.Resources.SilverSword;
+
+            // Обновляем ListBox
+            _inventory.UpdateListBox(listBox1);
+
+            listBox1.MouseDoubleClick += ItemsListBox_MouseDoubleClick;
+            SwordButton.Click += SwordButton_Click;
+            ArmorButton.Click += ArmorButton_Click;
+            PotionButton.Click += PotionButton_Click;
+            BombButton.Click += BombButton_Click;
+        }
+
+        //Обработчик нажатия на предмет в списке
+        private void ItemsListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.IndexFromPoint(e.Location);
+
+            if (index != ListBox.NoMatches && index < _inventory.Count)
+            {
+                Item selectedItem = _inventory[index];
+
+                // Используем предмет при двойном клике
+                switch (selectedItem)
+                {
+                    case Sword sword:
+                        SetSelectedSword(sword);
+                        break;
+
+                    case Bomb bomb:
+                        SetSelectedBomb(bomb);
+                        break;
+
+                    case Potion potion:
+                        SetSelectedPotion(potion);
+                        break;
+
+                    case Armor armor:
+                        SetSelectedArmor(armor);
+                        break;
+
+                    default:
+                        throw new IndexOutOfRangeException("Неопознанный предмет");
+                }
+            }
+        }
+
+        // Установка выбранного предмета
+        private void SetSelectedSword(Sword sword)
+        {
+            _selectedSword = sword;
+            SwordButton.BackgroundImage = sword.Icon;
+        }
+        private void SetSelectedBomb(Bomb bomb)
+        {
+            _selectedBomb = bomb;
+            BombButton.BackgroundImage = bomb.Icon;
+        }
+        private void SetSelectedPotion(Potion potion)
+        {
+            _selectedPotion = potion;
+            PotionButton.BackgroundImage = potion.Icon;
+        }
+        private void SetSelectedArmor(Armor armor)
+        {
+            _selectedArmor = armor;
+            ArmorButton.BackgroundImage = armor.Icon;
+        }
+
+        // Обработка нажатий
+        private void SwordButton_Click(object sender, EventArgs e)
+        {
+            if (_selectedSword != null)
+                TextBoxLog.AppendText(_selectedSword.Attack() + "\r\n");
+        }
+        private void ArmorButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_selectedArmor != null)
+                    TextBoxLog.AppendText(_selectedArmor.Fix() + "\r\n");
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Броня не нуждается в починке");
+            }
+        }
+        private void PotionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_selectedPotion != null)
+                    TextBoxLog.AppendText(_selectedPotion.Drink() + "\r\n");
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Нет зелий этого типа");
+            }
+
+            _inventory.UpdateListBox(listBox1);
+        }
+        private void BombButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_selectedBomb != null)
+                    TextBoxLog.AppendText(_selectedBomb.Attack() + "\r\n");
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Нет бомб этого типа");
+            }
+
+            _inventory.UpdateListBox(listBox1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
